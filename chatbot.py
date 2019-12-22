6901c5e693e382dddc62d9cc74eaea1928c5c029
David <dalau6@gmail.com>
2019-12-22 15:48:04 -0800

    refactored chatbot.

--- chatbot.py
+++ chatbot.py
@@ -1,41 +1,52 @@
-import nltk
-import numpy as np
+#import necessary libraries
+import io
 import random
 import string # to process standard python strings
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
 
-f=open('chatbot.txt','r',errors = 'ignore')
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True) # for downloading packages
 
-raw=f.read()
+# uncomment the following only the first time
+#nltk.download('punkt') # first-time use only
+#nltk.download('wordnet') # first-time use only
 
-raw=raw.lower()# converts to lowercase
 
-nltk.download('punkt') # first-time use only
-nltk.download('wordnet') # first-time use only
+#Reading in the corpus
+with open('chatbot.txt','r', encoding='utf8', errors ='ignore') as fin:
+    raw = fin.read().lower()
 
+#TOkenisation
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
 
-lemmer = nltk.stem.WordNetLemmatizer()
-
-#WordNet is a semantically-oriented dictionary of English included in NLTK.
+# Preprocessing
+lemmer = WordNetLemmatizer()
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
-
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
-
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
 
+# Keyword Matching
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 
 def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
     for word in sentence.split():
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
 
+
+# Generating response
 def response(user_response):
     robo_response=''
     sent_tokens.append(user_response)
@@ -52,6 +63,7 @@ def response(user_response):
     else:
         robo_response = robo_response+sent_tokens[idx]
         return robo_response
+
 
 flag=True
 print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
@@ -71,4 +83,4 @@ while(flag==True):
                 sent_tokens.remove(user_response)
     else:
         flag=False
-        print("ROBO: Bye! take care..")
+        print("ROBO: Bye! take care..")    
